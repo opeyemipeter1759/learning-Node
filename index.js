91adcd31518eb1fc9fe4141873d4841f6307ebc3
opeyemipeter <opeyemi@dufuna.com>
2021-10-22 15:07:45 +0100

    syncronous file system

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const { readFileSync, writeFileSync } = require('fs')
+const { readFileSync, writeFileSync } = require('fs');
+console.log(readFileSync);
+const first = readFileSync('./Content/first.txt', 'utf8');
+const second = readFileSync('./Content/second.txt', 'utf8');
+
+console.log(first + '   ' + second);
+writeFileSync('./Content/merged.txt', first + '\n' + second);
