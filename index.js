7a8a038328956f3e0c8b70aeb92144a1274198d8
opeyemipeter <opeyemi@dufuna.com>
2021-10-23 14:30:34 +0100

    some js nugget

---
+++ index.js
@@ -0,0 +1,13 @@
+const makeUpperCase = (name) => {
+    console.log(name.toUpperCase())
+}
+const reverseStr = (value) => {
+    console.log(value.split('').reverse('').join(''))
+}
+makeUpperCase('Opeyemi')
+const callBackSample = (name, callBackfn) => {
+    const fullName = `${name} Peter`
+    callBackfn(fullName)
+}
+
+callBackSample('Opeyemi', makeUpperCase)
